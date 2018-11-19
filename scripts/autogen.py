af11300b89e497dd04b6d90e87c58f538eb341ed
Rafael Cunha de Almeida <almeidaraf@gmail.com>
2018-11-19 15:43:10 +0000

    pyformat

--- scripts/autogen.py
+++ scripts/autogen.py
@@ -13,9 +13,9 @@ def main(text, source):
     with open(text) as fp:
         txt = fp.read()
 
-    nlp = spacy.load('pt_core_news_sm')
+    nlp = spacy.load("pt_core_news_sm")
 
-    doc = nlp(txt, disable=['ner'])
+    doc = nlp(txt, disable=["ner"])
 
     df = Counter()
     for token in doc:
@@ -34,8 +34,48 @@ def process_sentence(df, source, sent):
         return '"{} ({})","{}"'.format(sentence, source, answer)
 
 
+def post_process(sent, answer):
+    marker = "_" * 10
+
+    start = answer[0].idx - sent.start_char
+    end = (answer[-1].idx - sent.start_char) + len(answer[-1].text)
+    new_sent = sent.text[:start] + marker + sent.text[end:]
+
+    new_answer = sent.text[start:end]
+
+    # Fix extra spaces
+    new_sent = re.sub(r"\s+", " ", new_sent).strip()
+
+    # Add any - before or after ____ to the answer
+    left, right = re.search(r"(\w+-)?_+(-\w+)?", new_sent).groups()
+    if left:
+        new_answer = left + new_answer
+    if right:
+        new_answer += right
+
+    # Now replace the xxx-___-xxx pattern with ___
+    new_sent = re.sub(r"(\w+-)?(_+)(-\w+)?", r"\2", new_sent)
+
+    return new_sent, new_answer
+
+
+def choose_token(df, sent):
+    candidates = []
+    deps = ["nsubj", "nsubjpass", "dobj", "iobj", "ccomp", "xcomp"]
+    for token in sent:
+        if token.dep_ == "ROOT":
+            candidates.append(expand(token))
+        elif token.dep_ in deps and good_answer(token.text):
+            candidates.append(expand(token))
+
+    def dfsum(tokens):
+        return sum(df.get(t, -1) for t in tokens)
+
+    return heapq.nlargest(1, candidates, key=dfsum)[0]
+
+
 def expand(token):
-    deps = ['det', 'expl', 'advmod', 'nummod', 'compound']
+    deps = ["det", "expl", "advmod", "nummod", "compound"]
     expanded = [token]
     for left in reversed(list(token.lefts)):
         if left.i == (expanded[0].i - 1) and left.dep_ in deps:
@@ -50,56 +90,19 @@ def expand(token):
     return expanded
 
 
-def choose_token(df, sent):
-    candidates = []
-    for token in sent:
-        if token.dep_ == 'ROOT':
-            candidates.append(expand(token))
-        elif token.dep_ in ['nsubj', 'nsubjpass', 'dobj', 'iobj', 'ccomp', 'xcomp'] and good_answer(token.text):
-            candidates.append(expand(token))
-
-    def dfsum(tokens):
-        return sum(df.get(t, -1) for t in tokens)
-
-    return heapq.nlargest(1, candidates, key=dfsum)[0]
-
-
-def post_process(sent, answer):
-    marker = '_' * 10
-
-    start = answer[0].idx - sent.start_char
-    end = (answer[-1].idx - sent.start_char) + len(answer[-1].text)
-    new_sent = sent.text[:start] + marker + sent.text[end:]
-
-    new_answer = sent.text[start:end]
-
-    # Fix extra spaces
-    new_sent = re.sub(r'\s+', ' ', new_sent).strip()
-
-    # Add any - before or after ____ to the answer
-    left, right = re.search(r'(\w+-)?_+(-\w+)?', new_sent).groups()
-    if left:
-        new_answer = left + new_answer
-    if right:
-        new_answer += right
-
-    # Now replace the xxx-___-xxx pattern with ___
-    new_sent = re.sub(r'(\w+-)?(_+)(-\w+)?', r'\2', new_sent)
-
-    return new_sent, new_answer
-
-
 def good_answer(answer):
     return answer and len(answer) > 3
 
 
 def is_ok(sentence, answer):
-    return good_answer(answer) \
-        and sentence[0] != '_' \
-        and sentence[0] == sentence[0].upper() \
-        and (sentence[0] not in ('.', ';', '!', '?')) \
-        and (sentence[-1] in ('.', ';', '!', '?')) \
+    return (
+        good_answer(answer)
+        and sentence[0] != "_"
+        and sentence[0] == sentence[0].upper()
+        and (sentence[0] not in (".", ";", "!", "?"))
+        and (sentence[-1] in (".", ";", "!", "?"))
         and len(sentence) > 50
+    )
 
 
 if __name__ == "__main__":
